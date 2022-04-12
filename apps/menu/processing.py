80fc7daa159c42965d609da3d20ff6aa99ac1d41
Thomas <thomas.juldo@edu.devinci.fr>
2022-04-12 09:13:41 +0200

    Using correct driver

--- apps/menu/processing.py
+++ apps/menu/processing.py
@@ -5,11 +5,11 @@ class Application(BaseApplication):
 
     def __init__(self, name, hal, server, manager):
         super().__init__(name, hal, server, manager)
-        self.requires["pose"] = ["raw_data"]
+        self.requires["pose_to_mirror"] = ["mirrored_data"]
 
     def listener(self, source, event, data):
         super().listener(source, event, data)
 
-        if source == "pose" and event == "raw_data":
+        if source == "pose_to_mirror" and event == "mirrored_data":
             self.data = data
             self.server.send_data(self.name, self.data)
